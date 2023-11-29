f589bdbaff76255a6c091a831c063c43d4e878d1
Jonathan Colombo <jonathancolombo98@outlook.com>
2023-11-29 22:43:46 +0100

    Create basics.m
    Alcune istruzioni basiche

---
+++ basics.m
@@ -0,0 +1,19 @@
+clc
+clearvars
+x = 10;
+y = x + 7;
+w = x + y;
+clearvars
+
+%Comments
+x = 10;
+y = 10 + x; % in-line
+%Very useful
+
+%Matrices and Vectors
+clc, clearvars
+
+x = 1:10;
+x';
+x = linspace(20,50); % 100 values between 20 and 50
+x = linspace(20,100,20); % 20 values between 20 and 100
